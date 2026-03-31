9affa1c22eba5a54e7402f4d28dc25b723259b95
ricardoshelly-wq <ricardoshelly@gmail.com>
2026-03-30 20:07:50 -0400

    Enhance app with deployment and usage instructions
    Added deployment and usage instructions for Streamlit app.
    
    Signed-off-by: ricardoshelly-wq <ricardoshelly@gmail.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write(
+    "When you're ready to deploy, check out [Streamlit Cloud](https://streamlit.io/cloud) - it's free and super easy to use! 🚀"
+)
+st.write(
+    "I won't be able to run this code, but you can! Just save it as `streamlit_app.py` and run `streamlit run streamlit_app.py` in your terminal. Happy coding!"
+)
